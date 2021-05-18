d6aaf8a9b14e28c947fe88f3f6132d734e6d7913
Riya Acharya <ria.acharya07@gmail.com>
2021-05-18 18:27:51 +0545

    image asset added and loaded on screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,31 @@
+import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
 
-
- 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                
+                Center(
+                  child: CircleAvatar(
+                    backgroundColor: Colors.teal,
+                    radius: 75.0,
+                    foregroundImage: AssetImage('images/avatar.png'),
+                  ),
+                ),
+              ],
+          ),
+        ),
+      ),
+    );
+  }
+}
