92f0383df2353df65dfece3cf4a6118121ee8a8b
Riya Acharya <ria.acharya07@gmail.com>
2021-05-22 17:38:36 +0545

    card widget updated

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,73 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                
-                Center(
-                  child: CircleAvatar(
-                    backgroundColor: Colors.teal,
-                    radius: 75.0,
-                    foregroundImage: AssetImage('images/avatar.png'),
+          child: Column(
+            children: [
+              CircleAvatar(
+                radius: 80.0,
+                backgroundColor: Colors.teal,
+                backgroundImage: AssetImage('images/avatar.png'),
+              ),
+              Text(
+                'Angela Carli',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'SOFTWARE ENGINEER',
+                style: TextStyle(
+                  fontFamily: 'Source Sans Pro',
+                  fontSize: 20.0,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(Icons.phone, color: Colors.teal),
+                  
+                  title: Text(
+                    '+44 123 456 789',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                    ),
                   ),
                 ),
-              ],
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'angelacarli7@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
